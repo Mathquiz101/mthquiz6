0525b5c35e773c42126bb4c99e3af0864b6b153f
Mathquiz101 <mathquiz202@gmail.com>
2024-12-17 22:16:31 +0000

    Update script.js

--- script.js
+++ script.js
@@ -175,7 +175,7 @@ function endQuiz() {
     if (percentage > 50) {
         const nextLevelContainer = document.getElementById('next-level-container');
         const nextLevelLink = document.getElementById('next-level-link');
-        const link = 'https://waecmathsuccess.github.io/mathlevel6/';
+        const link = 'https://mathquiz101.github.io/mathquizzy7/';
         
         nextLevelLink.href = link;
         nextLevelLink.textContent = link;
